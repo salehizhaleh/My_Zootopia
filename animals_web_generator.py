b8aee632d38883745df864cac958ae12ee38204f
Zohreh <salehii.zhalehl@gmail.com>
2026-06-25 22:41:15 +0200

    Complete step 2 - write data to HTML file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,16 +7,34 @@ def load_data(file_path):
     return json.load(handle)
 
 
+def read_html(file_path):
+  with open(file_path, "r") as handle:
+    return handle.read()
+
+
+def write_html(file_path, content):
+  with open(file_path, "w") as handle:
+    handle.write(content)
+
+html_content = read_html("animals_template.html")
 animals_data = load_data('animals_data.json')
 
+
+
+output = ''
 for animal in animals_data:
   if "name" in animal:
-    print(f'Name: {animal["name"]}')
+    output += f'Name: {animal["name"]}\n'
   if "diet" in animal["characteristics"]:
-    print(f'Diet: {animal["characteristics"]["diet"]}')
+    output += f'Diet: {animal["characteristics"]["diet"]}\n'
   if "locations" in animal:
-    print(f'Location: {animal["locations"][0]}')
+    output += f'Location: {animal["locations"][0]}\n'
   if "type" in animal["characteristics"]:
-    print(f'Type: {animal["characteristics"]["type"]}')
-  print()
+    output += f'Type: {animal["characteristics"]["type"]}\n'
+  output += "\n"
 
+
+final_html = html_content.replace("__REPLACE_ANIMALS_INFO__", output)
+
+
+write_html("animals.html", final_html)
